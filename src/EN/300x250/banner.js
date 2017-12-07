eb4d4d4452bbaa12c5a55bad2dce30e4c3177474
peerum.you@campjefferson.com <peerum.you@ws-to-wm-p4658.identification7.com>
2017-12-07 15:37:04 -0500

    created empty clean copy

--- src/EN/300x250/banner.js
+++ src/EN/300x250/banner.js
@@ -3,7 +3,6 @@ import { getElements, getElement } from "common/getElements";
 (function() {
   var elements = {};
   var clickarea = document.getElementById("clickarea");
-  var cta = document.getElementById("cta");
 
   var domReady = function(callback) {
     document.readyState === "interactive" || document.readyState === "complete"
@@ -23,33 +22,11 @@ import { getElements, getElement } from "common/getElements";
   }
 
   function rollover() {
-    TweenLite.to(chevron, 0.25, { x: 2 });
   }
 
   function rollout() {
-    TweenLite.to(chevron, 0.25, { x: 0 });
   }
 
   function animate() {
-    TweenLite.from(getElement("copy1"), 0.5, { alpha: 0 });
-    TweenLite.to("#background-1", 5, { x: 20 });
-    TweenLite.to(getElement("copy1"), 0.5, { alpha: 0, delay: 2 });
-
-    TweenLite.from(frame2, 0.5, { alpha: 0, delay: 2.5 });
-    TweenLite.to("#background-2", 5, { x: -20, delay: 2.5 });
-
-    TweenLite.from(frame3, 0.5, { alpha: 0, delay: 5.5 });
-    TweenLite.to("#background-3", 5, { x: 20, delay: 5.5 });
-
-    TweenLite.from(frame4, 0.5, { alpha: 0, delay: 8.5 });
-    TweenLite.from("#background-4", 5, { scale: 1.1, delay: 8.5 });
-    TweenLite.from(getElement("copy2"), 0.5, { alpha: 0, delay: 9 });
-    TweenLite.to(getElement("copy2"), 0.5, { alpha: 0, delay: 11.5 });
-
-    TweenLite.from(frame5, 0.5, { alpha: 0, delay: 12 });
-    TweenLite.to("#background-5", 3, { x: -20, delay: 12 });
-    TweenLite.from(getElement("copy3"), 0.5, { alpha: 0, delay: 12.5 });
-    TweenLite.from(cta, 0.5, { alpha: 0, delay: 12.5 });
-    TweenLite.from(logo, 0.5, { y: 100, delay: 13 });
   }
 })();
